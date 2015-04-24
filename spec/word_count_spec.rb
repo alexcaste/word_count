287b532886f889a5d9d3c27d54e135f3f07574c0
Alex Caste <alex.castaneda@gmail.com>
2015-04-24 09:50:39 -0700

    wrote spec to split sentence into array of single words

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -1,3 +1,16 @@
 require('rspec')
 require('pry')
 require('counter')
+
+describe('String#.word_counter') do
+
+  #it takes a sentence and splits it into individual words
+    it("takes a input sentence and splits into individual words") do
+      expect("the dog is brown".word_counter()).to(eq(sentence array = [ "the", "dog", "is", "brown"]))
+    end
+
+  #it takes a word and finds it within a sentence
+
+  #it counts how many times a word is within a sentence
+
+end
