24d455cfa94bb3377401438155c6e20a6c9d0917
Alex Caste <alex.castaneda@gmail.com>
2015-04-24 10:44:20 -0700

    successfully counts the number of times a word appears within a sentence

--- lib/counter.rb
+++ lib/counter.rb
@@ -2,15 +2,16 @@ class String
   define_method(:word_counter) do |input_word|
     input_sentence = self
     sentence_array = input_sentence.split(/\W+/)
+    counter = 0
 
     sentence_array.each() do |compare_word|
       if compare_word == input_word
-        return true
+          counter = counter.+(1)
       else
       end
 
     end
-
+    return counter
   end
 
 end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -6,17 +6,17 @@ describe('String#.word_counter') do
 
   #it takes a sentence and splits it into individual words
     it("takes a input sentence and splits into individual words") do
-      expect("the dog is brown".word_counter("")).to(match_array(["the","dog", "is", "brown"]))
+      expect("the dog is brown".word_counter("")).to(eq(0))
     end
 
   #it takes a word and finds it within a sentence
     it("takes a given word and returns true if word is within sentence") do
-      expect("the dog is brown".word_counter("dog")).to(eq(true))
+      expect("the dog is brown".word_counter("dog")).to(eq(1))
     end
 
   #it counts how many times a word is within a sentence
     it("takes a given word and a sentence returns the number of times that word appears within the sentence") do
-      expect("the dog is brown, brown".word_count("brown")).to(eq(2))
+      expect("the dog is brown, brown".word_counter("brown")).to(eq(2))
     end
 
 end
