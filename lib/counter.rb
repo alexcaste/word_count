8af9944642b048f8b72d8e3ba58c357a9b602404
Alex Caste <alex.castaneda@gmail.com>
2015-04-24 10:37:05 -0700

    successfully compares a word to a sentence and returns true if word is within sentence

--- lib/counter.rb
+++ lib/counter.rb
@@ -1,6 +1,16 @@
 class String
-  define_method(:word_counter) do
+  define_method(:word_counter) do |input_word|
     input_sentence = self
     sentence_array = input_sentence.split(/\W+/)
+
+    sentence_array.each() do |compare_word|
+      if compare_word == input_word
+        return true
+      else
+      end
+
+    end
+
   end
+
 end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -6,7 +6,7 @@ describe('String#.word_counter') do
 
   #it takes a sentence and splits it into individual words
     it("takes a input sentence and splits into individual words") do
-      expect("the dog is brown".word_counter()).to match_array(["the","dog", "is", "brown"])
+      expect("the dog is brown".word_counter("")).to(match_array(["the","dog", "is", "brown"]))
     end
 
   #it takes a word and finds it within a sentence
